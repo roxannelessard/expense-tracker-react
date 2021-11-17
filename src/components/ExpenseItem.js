bec0780a92a89c6fbbdc19a22adca988f2a1d9e6
roxanne lessard <me@rlessard.com>
2021-11-17 14:49:44 -0500

    feat: creates date component and add css stylesheet for it

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,11 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
-  // Add .toISOString() because it is a date object
-  const month = props.date.toLocaleString("en-us", { month: "long" });
-  const year = props.date.getFullYear();
-  const day = props.date.toLocaleString("en-us", { day: "2-digit" });
 
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
       </div>
