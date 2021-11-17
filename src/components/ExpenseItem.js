4a801c3ffce278a27e49c63ef09f9a7421d025a0
roxanne lessard <me@rlessard.com>
2021-11-17 14:29:18 -0500

    fix: formats year,month,day with LocaleString and getFullYear

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,18 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  // Add .toISOString() because it is a date object
+  const month = props.date.toLocaleString("en-us", { month: "long" });
+  const year = props.date.getFullYear();
+  const day = props.date.toLocaleString("en-us", { day: "2-digit" });
 
   return (
     <div className="expense-item">
-      {/* Add .toISOString() because it is a date object */}
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
       </div>
