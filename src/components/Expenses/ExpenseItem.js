75bbb52cdc0ecddc3a816a4c0ac487176fb7fbf6
roxanne lessard <me@rlessard.com>
2021-11-18 17:41:23 -0500

    feat: adds change title btn and state documentation

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,31 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
+import React, { useState } from 'react';
 
 const ExpenseItem = (props) => {
+  // with use state we're creating a variable which will lead to call this function again
+  // props.title is the default value
+  // it returns a function which we can then call to assign a new value
+  // (we'll be assigning new values by calling a function) for that UseState returns an array [currentStateValue, updatingFunction]
+  // so we use array destructuring to store the elements
+  const [title, setTitle] = useState(props.title);
 
+  const clickHandler = () => {
+    // by calling setTitle() the component return by the function will be executed again!
+    setTitle('Updated');
+    console.log(title);
+  }
   return (
     // you can't use your custom components as wrappers around other kind of content
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
       </div>
       <div className="expense-item__price">{props.amount}</div>
+      {/* you write clickHandler without the () because you just want to point at the function & not execute the code right now */}
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 }
