ab9cf7d310bc1ba27ff8e6de2b362a9aa4a2ffc1
roxanne lessard <me@rlessard.com>
2021-11-29 22:03:20 -0500

    fix: debugged the add expense report

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+import React, { useState } from "react";
 
-const App = () => {
-  const expenses = [
+  const dummyData = [
     {
       id: "e1",
       title: "Pour Over Kettle",
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: "e2",
-    title: "New TV",
-    amount: 799.49,
-    date: new Date(2021, 2, 12) },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
     {
       id: "e3",
       title: "Health Insurance",
@@ -27,10 +24,17 @@ const App = () => {
     },
   ];
 
-  const addExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense);
-  }
+
+const App = () => {
+  const [expenses, setExpenses] = useState(dummyData);
+
+  const addExpenseHandler = (expense) => {
+    // setExpenses([expense, ...expenses]);
+    // Correct way below: we will use a function to use the latest state snapshot (because this depends from previous state)
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
 
   return (
     <div>
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,7 +1,7 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
 const ExpenseItem = (props) => {
   // with use state we're creating a variable which will lead to call this function again
@@ -10,23 +10,23 @@ const ExpenseItem = (props) => {
   // (we'll be assigning new values by calling a function) for that UseState returns an array [currentStateValue, updatingFunction]
   // so we use array destructuring to store the elements
   // state is separated on a per component basis
-  const [title, setTitle] = useState(props.title);
+  // const [title, setTitle] = useState(props.title);
 
-  const clickHandler = () => {
-    // by calling setTitle() the component return by the function will be executed again!
-    setTitle("Updated");
-    console.log(title);
-  };
+  // const clickHandler = () => {
+  //   // by calling setTitle() the component return by the function will be executed again!
+  //   setTitle("Updated");
+  //   console.log(title);
+  // };
+
   return (
     // you can't use your custom components as wrappers around other kind of content
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
       </div>
       <div className="expense-item__price">{props.amount}</div>
       {/* you write clickHandler without the () because you just want to point at the function & not execute the code right now */}
-      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 }
