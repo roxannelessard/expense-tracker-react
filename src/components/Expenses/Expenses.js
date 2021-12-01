f02cda7482dc78c61023251f457543a5b7e1e877
roxanne lessard <me@rlessard.com>
2021-12-01 17:30:16 -0500

    fix: corrects the ExpenseFilter and adds ExpensesList

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,18 +1,17 @@
 import "./Expenses.css";
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
-import { useState } from 'react';
+import { useState } from "react";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2021");
 
-  const [filteredYear, setFilteredYear] = useState('2021');
-
-  const filterChangeHandler = selectedYear => {
+  const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
 
-  const filteredExpenses = props.items.filter(expense => {
+  const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
@@ -23,20 +22,10 @@ const Expenses = (props) => {
           onChangeFilter={filterChangeHandler}
           selected={filteredYear}
         />
-        {filteredExpenses.length === 0 && <p>No expenses found</p>}
-        {filteredExpenses.length > 0 &&
-          filteredExpenses.map((expense) => (
-            <ExpenseItem
-              // for performance purpose : this key help react to find the individual item that changed (if not react would update all content not just s specific thing)
-              key={expense.id}
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-            />
-          ))}
+        <ExpensesList items={filteredExpenses}/>
       </Card>
     </div>
   );
-}
+};
 
 export default Expenses;
